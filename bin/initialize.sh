26c2f48fc64202fc495d8acaa6abc3f50086c694
Evgeny Padniuk <lord_rb@mail.ru>
2019-02-25 02:02:10 +0000

    Small fixes

--- bin/initialize.sh
+++ bin/initialize.sh
@@ -1,32 +1,6 @@
 #!/bin/bash
 
-docker network create --ipv6 --driver=bridge --subnet=172.20.0.0/16 --subnet=2002:ac14:0000::/48 --gateway=172.20.0.1 proxy
-
-#
-
-#cd ~
-#cat ~/.ssh/hassio.pub | cat >> ~/.ssh/authorized_keys
-
-#chmod 700 .ssh
-#chmod 600 .ssh/authorized_keys
-
-#sudo apt-get install mc nano htop bmon
-
-#sudo usermod -aG homeassistant $USER
-
-#sudo chmod -R g+rwX /home/homeassistant
-
-#yes | cp -rf ~/cfg/* /home/homeassistant/.homeassistant/
-
-#sudo chown -R homeassistant:homeassistant /srv/homeassistant
-
 #sudo apt-get install vlc
-#sudo usermod -a -G audio homeassistant
-
-#sudo systemctl restart home-assistant@homeassistant
-
-#install hassctl
-#sudo curl -o /usr/local/bin/hassctl https://raw.githubusercontent.com/dale3h/hassctl/master/hassctl && sudo chmod +x /usr/local/bin/hassctl
 
 #install MQTT broker
 #wget 'http://repo.mosquitto.org/debian/mosquitto-repo.gpg.key' -O ./mosquitto-repo.gpg.key -q && sudo apt-key add ./mosquitto-repo.gpg.key
